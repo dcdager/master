54555a3df681dbdf38481d9580df085211fa2917
daniel colmenares <danieldager@gmail.com>
2023-02-06 19:00:17 +0100

    Version funcional

--- src/app/components/blogcomp/blogcomp.component.ts
+++ src/app/components/blogcomp/blogcomp.component.ts
@@ -8,19 +8,19 @@ import { Component } from '@angular/core';
 export class BlogcompComponent {
   title : string = "";
   atext : string = "";
-  afecha : Date = new Date();
+  afecha : string = "";
   aurl : string = "";
   arrArticles : any[] = [];
 
   constructor() {
     this.arrArticles = [
-      { titulo : "Daniel",
-        texto : "Articulo de daniel",
-        fecha : new Date(2021,4,20) ,
+      { titulo : "¿Qué es Lorem Ipsum?",
+        texto : "Es simplemente el texto de relleno de las imprentas y archivos de texto. Lorem Ipsum ha sido el texto de relleno estándar de las industrias desde el año 1500, cuando un impresor (N. del T. persona que se dedica a la imprenta) desconocido usó una galería de textos y los mezcló de tal manera que logró hacer un libro de textos especimen. No sólo sobrevivió 500 años, sino que tambien ingresó como texto de relleno en documentos electrónicos, quedando esencialmente igual al original. Fue popularizado en los 60s con la creación de las hojas Letraset, las cuales contenian pasajes de Lorem Ipsum, y más recientemente con software de autoedición, como por ejemplo Aldus PageMaker, el cual incluye versiones de Lorem Ipsum.",
+        fecha : "2021-4-20" ,
         url : "https://via.placeholder.com/150",},
-      { titulo : "Rafael",
-        texto : "Articulo de Rafael",
-        fecha: new Date(2022,4,21),
+      { titulo : "¿Por qué lo usamos?",
+        texto : "Es un hecho establecido hace demasiado tiempo que un lector se distraerá con el contenido del texto de un sitio mientras que mira su diseño. El punto de usar Lorem Ipsum es que tiene una distribución más o menos normal de las letras, al contrario de usar textos como por ejemplo Contenido aquí, contenido aquí. Estos textos hacen parecerlo un español que se puede leer. Muchos paquetes de autoedición y editores de páginas web usan el Lorem Ipsum como su texto por defecto, y al hacer una búsqueda de Lorem Ipsum va a dar por resultado muchos sitios web que usan este texto si se encuentran en estado de desarrollo. Muchas versiones han evolucionado a través de los años, algunas veces por accidente, otras veces a propósito (por ejemplo insertándole humor y cosas por el estilo).",
+        fecha: "2022-4-21",
         url:"https://via.placeholder.com/150",
      }
 
@@ -34,10 +34,11 @@ export class BlogcompComponent {
       fecha: this.afecha,
       url: this.aurl
     }
+    console.log(this.afecha);
     this.arrArticles.push(article);
     this.title = "";
     this.atext = "";
-    this.afecha = new Date();
+    this.afecha = "";
     this.aurl = "";
 
   }
